ac477fcb291c656213309662528cdf4adba7bd55
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-10-31 20:34:22 +0100

    Finalized iteration5

---
+++ models/Movie.model.js
@@ -0,0 +1,14 @@
+//  Add your code here
+const Celebrity = require('../models/Celebrity.model')
+const { Schema, model } = require('mongoose')
+
+
+const movieSchema = new Schema ({
+    title: String,
+    genre: String,
+    plot: String,
+    cast: [ { type: Schema.Types.ObjectId, ref: 'Celebrity' } ]
+})
+
+const Movie = model('Movie', movieSchema);
+module.exports = Movie;
