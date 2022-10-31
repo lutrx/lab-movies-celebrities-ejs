973d7d756406c6af47bf08b3c9f1aab8a49aa7a9
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-10-31 17:53:32 +0100

    Finalized iteration3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,31 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require('express').Router()
+const Celebrity = require('../models/Celebrity.model')
 
-// all your routes here
+//Get celebrities
+
+router.get('/', (req, res, next) => {
+    res.render('celebrities/celebrities')
+})
+
+//Render the view for creating a celebrity
+router.get('/create', (req, res, next) => {
+    res.render('celebrities/new-celebrity')
+})
+
+//Create the POST Method
+router.post('/create', async (req, res) => {
+    try {
+      await Celebrity.create({
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase
+      })
+      res.redirect('/celebrities')  
+    } catch (error) {
+        console.log(error)
+        res.render('celebrities/new-celebrity')
+    }
+})
 
 module.exports = router
